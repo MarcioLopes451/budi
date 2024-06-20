9abbb26557382bff6afcadc0870570a31af7d97e
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2024-06-20 16:33:06 +0100

    add: mobile nav

---
+++ src/components/Navbar/MobileNav.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+type Nav = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export default function MobileNav({ isOpen, onClose }: Nav) {
+  const closeNav = () => {
+    return onClose();
+  };
+  return isOpen ? (
+    <div className="bg-SeaShell mt-10 z-50 w-full fixed left-0">
+      <div className="flex justify-start items-start px-[24px] font-Poppins font-bold flex-col gap-5">
+        <Link to={"/"}>what we do</Link>
+        <Link to={"/"}>find a budi</Link>
+        <Link to={"/"}>be a budi</Link>
+        <Link to={"/"}>our story</Link>
+        <Link to={"/"}>contact us</Link>
+        <Link to={"/"}>real stories</Link>
+      </div>
+      <button onClick={closeNav}>close</button>
+    </div>
+  ) : null;
+}
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,23 @@
 import Logo from "../../images/blackLogo.png";
 import UserImg from "../../images/1564534_customer_man_user_account_profile_icon (1).png";
 import MobileMenuImg from "../../images/4781852_burger_line_list_menu_nav_icon.png";
+import { useState } from "react";
+import MobileNav from "./MobileNav";
+
 export default function Navbar() {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
   return (
     <div className="bg-SeaShell h-[120px] px-[24px] pt-[30px]">
       <div className="flex justify-between items-center">
         <img src={UserImg} className="w-[40px]" />
         <img src={Logo} className="w-[110px]" />
-        <img src={MobileMenuImg} className="w-[40px]" />
+        <img src={MobileMenuImg} className="w-[40px]" onClick={handleClick} />
       </div>
+      {<MobileNav isOpen={open} onClose={handleClick} />}
     </div>
   );
 }
